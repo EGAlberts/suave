9c851e4507ce0c936bafe29b0b8b195c7a2db850
Elvin <EGAlberts@github.com>
2023-12-17 15:45:18 +0100

    refactoring for working with rebet

--- suave/suave/follow_pipeline_lc.py
+++ suave/suave/follow_pipeline_lc.py
@@ -1,11 +1,14 @@
+
 #!/usr/bin/env python
+from typing import Optional
+
 import math
 import rclpy
 import threading
 
-from rclpy.lifecycle import Node
-from rclpy.lifecycle import State
-from rclpy.lifecycle import TransitionCallbackReturn
+from rclpy.node import Node
+from rclpy.action import ActionServer
+from suave_msgs.action import Follow
 
 from suave_msgs.srv import GetPath
 from suave.bluerov_gazebo import BlueROVGazebo
@@ -14,65 +17,67 @@ from std_msgs.msg import Bool
 from std_msgs.msg import Float32
 
 
-class PipelineFollowerLC(Node):
+class PipelineFollowerAC(Node):
 
     def __init__(self, node_name, **kwargs):
         super().__init__(node_name, **kwargs)
-        self.trigger_configure()
         self.abort_follow = False
         self.distance_inspected = 0
 
-    def on_configure(self, state: State) -> TransitionCallbackReturn:
-        self.get_logger().info("on_configure() is called.")
         self.ardusub = BlueROVGazebo('bluerov_pipeline_follower')
         self.thread = threading.Thread(
             target=rclpy.spin, args=(self.ardusub, ), daemon=True)
         self.thread.start()
 
+        self._action_server = ActionServer(
+            self,
+            Follow,
+            'follow',
+            self.execute_callback)
         self.get_path_timer = self.create_rate(5)
         self.get_path_service = self.create_client(
             GetPath, 'pipeline/get_path')
 
-        self.pipeline_inspected_pub = self.create_lifecycle_publisher(
+        self.pipeline_inspected_pub = self.create_publisher(
             Bool, 'pipeline/inspected', 10)
 
         self.pipeline_distance_inspected_pub = self.create_publisher(
             Float32, 'pipeline/distance_inspected', 10)
-        return TransitionCallbackReturn.SUCCESS
 
-    def on_activate(self, state: State) -> TransitionCallbackReturn:
+
+    def execute_callback(self, goal_handle):
+        self.get_logger().info('Executing goal...')
+
         self.get_logger().info("on_activate() is called.")
         if not self.get_path_service.wait_for_service(timeout_sec=1.0):
             self.get_logger().info(
                 'pipeline/get_path service is not available')
-            return TransitionCallbackReturn.FAILURE
+            goal_handle.abort()
+            return Follow.Result()
         if self.executor is None:
             self.get_logger().info('Executor is None')
-            return TransitionCallbackReturn.FAILURE
-        else:
-            self.executor.create_task(self.follow_pipeline)
-            self.abort_follow = False
+            goal_handle.abort()
+            return Follow.Result()
 
-        return super().on_activate(state)
+        
+        self.follow_pipeline()
 
-    def on_deactivate(self, state: State) -> TransitionCallbackReturn:
-        self.get_logger().info("on_deactivate() is called.")
-        self.abort_follow = True
-        return super().on_deactivate(state)
 
-    def on_cleanup(self, state: State) -> TransitionCallbackReturn:
-        self.get_logger().info('on_cleanup() is called.')
-        self.ardusub.destroy_node()
-        self.thread.join()
-        self.follow_pipeline_thread.join()
-        return TransitionCallbackReturn.SUCCESS
+        while not self.pipeline_detected:
+            pass
+            # self.get_logger().info("Waiting for pipleline to be detected..")
+        
+        goal_handle.succeed()
+        
+        result = Follow.Result()
 
-    def on_shutdown(self, state: State) -> TransitionCallbackReturn:
-        self.get_logger().info('on_shutdown() is called.')
-        self.ardusub.destroy_node()
-        self.thread.join()
-        self.follow_pipeline_thread.join()
-        return TransitionCallbackReturn.SUCCESS
+        
+        return result
+        
+
+
+
+
 
     def follow_pipeline(self):
         self.get_logger().info("Follow pipeline started")
@@ -80,27 +85,19 @@ class PipelineFollowerLC(Node):
         pipe_path = self.get_path_service.call_async(GetPath.Request())
 
         timer = self.ardusub.create_rate(5)  # Hz
+
         while not pipe_path.done():
-            if self.abort_follow is True:
-                return
             timer.sleep()
+
         last_point = None
         self.distance_inspected = 0
         for gz_pose in pipe_path.result().path.poses:
-            if self.abort_follow is True:
-                return
+
             setpoint = self.ardusub.setpoint_position_gz(
                 gz_pose, fixed_altitude=True)
 
             count = 0
             while not self.ardusub.check_setpoint_reached_xy(setpoint, 0.5):
-                if self.abort_follow is True:
-                    self.distance_inspected += self.calc_distance(
-                        last_point, self.ardusub.local_pos)
-                    dist = Float32()
-                    dist.data = self.distance_inspected
-                    self.pipeline_distance_inspected_pub.publish(dist)
-                    return
                 if count > 10:
                     setpoint = self.ardusub.setpoint_position_gz(
                         gz_pose, fixed_altitude=True)
@@ -120,6 +117,8 @@ class PipelineFollowerLC(Node):
         self.pipeline_inspected_pub.publish(pipe_inspected)
         self.get_logger().info("Follow pipeline completed")
 
+        return
+
     def calc_distance(self, pose1, pose2):
         return math.sqrt(
             (pose1.pose.position.x - pose2.pose.position.x)**2 +
@@ -130,7 +129,7 @@ def main():
     rclpy.init()
 
     executor = rclpy.executors.MultiThreadedExecutor()
-    lc_node = PipelineFollowerLC('f_follow_pipeline_node')
+    lc_node = PipelineFollowerAC('f_follow_pipeline_node')
     executor.add_node(lc_node)
     try:
         executor.spin()
